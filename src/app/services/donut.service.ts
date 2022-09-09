d1a3537aa01505356e3bf4fadea87e9f2b3e49f5
miraj <miraj00@gmail.com>
2022-09-08 23:19:12 -0400

    service created

---
+++ src/app/services/donut.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Donut, Result, Donuts } from '../interfaces/donut';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DonutService {  
+  constructor(private httpClient:HttpClient) { }
+
+  // function to get list of donuts from apiEndpoint : 
+  getDonutList(): Observable<Result[]>{
+    return this.httpClient.get<Donuts>(environment.apiEndpoint).pipe(
+      map((data)=>data.results)
+    );
+  }
+
+  // function to get detail list for donut from detailEndpoint : 
+  getDonutDetail(id:string): Observable<Donut>{
+    return this.httpClient.get<Donut>(environment.detailEndpoint(id));
+  }
+}
