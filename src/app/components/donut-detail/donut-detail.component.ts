d6636f0a8d5c61a2737865da647dc41c6a42946a
miraj <miraj00@gmail.com>
2022-09-09 00:03:30 -0400

    donut-detail component edited

--- src/app/components/donut-detail/donut-detail.component.ts
+++ src/app/components/donut-detail/donut-detail.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Donut, Result } from 'src/app/interfaces/donut';
+import { DonutService } from 'src/app/services/donut.service';
+
 
 @Component({
   selector: 'app-donut-detail',
   templateUrl: './donut-detail.component.html',
   styleUrls: ['./donut-detail.component.css']
 })
+
+
 export class DonutDetailComponent implements OnInit {
 
-  constructor() { }
+  donut:Donut | any;
+
+  constructor(private service:DonutService, private route:ActivatedRoute) {
+
+
+   }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( (params)=>{
+      this.getDonutDetail(String(params.get("id")));
+    });
+  }
+
+ //  function to get Donut Detail by Id using subscribe in here and observable in services
+  getDonutDetail(id:string){
+    this.service.getDonutDetail(id).subscribe((data)=>{
+      this.donut = data;
+    });
   }
 
 }
