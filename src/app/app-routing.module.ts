88abceda583cb28acc95ee9a00ba91c192c426f2
miraj <miraj00@gmail.com>
2022-09-09 00:58:47 -0400

    shopping cart edit in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DonutDetailComponent } from './components/donut-detail/donut-detail.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
 
   {path: 'home', component: HomePageComponent},
   {path: 'donutDetail/:id', component: DonutDetailComponent},
+  {path: 'shopping-cart/:id', component: ShoppingCartComponent},
   {path: '', redirectTo: '/home', pathMatch:'full'},
   {path: '**', component: NotFoundComponent}
 
--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Donut, Result } from 'src/app/interfaces/donut';
+import { DonutService } from 'src/app/services/donut.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  donut:Donut | any;
+
+  constructor(private service:DonutService, private route:ActivatedRoute) {
+
+
+   }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( (params)=>{
+      this.getDonutDetail(String(params.get("id")));
+    });
   }
+
+ //  function to get Donut Detail by Id using subscribe in here and observable in services
+  getDonutDetail(id:string){
+    this.service.getDonutDetail(id).subscribe((data)=>{
+      this.donut = data;
+    });
+  }
+
+  removeDonut(){
+ //    
+      
+
+  }
+
+
+
+
+
+
 
 }
